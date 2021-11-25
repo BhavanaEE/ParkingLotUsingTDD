6ab0c06df08ab1a04fdd8db3a61ea1a3c2ed6f8f
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-11-25 16:32:13 +0530

    Checked free slots and occupied slots test method in display car test class

--- src/test/java/com/everest/displaystrategy/DisplayCarStrategyTest.java
+++ src/test/java/com/everest/displaystrategy/DisplayCarStrategyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +24,23 @@ class DisplayCarStrategyTest {
     void checkDisplayFreeCount() {
         ArrayList<ArrayList<Integer>> expected=display.display(parkingLot,3,parkingLot.getSlotsInEachFloor());
          int actual=3;
-         assertEquals(expected.get(0).size(),actual);
+         int floorNumber=0;
+         assertEquals(expected.get(floorNumber).size(),actual);
     }
+    @Test
+    void checkDisplayFreeSlots() {
+        ArrayList<ArrayList<Integer>> expected=display.display(parkingLot,3,parkingLot.getSlotsInEachFloor());
+        ArrayList<Integer> actual=new ArrayList<Integer>(Arrays.asList(4,5,6));
+        int floorNumber=0;
+        assertEquals(expected.get(floorNumber),actual);
+    }
+
+    @Test
+    void checkDisplayOccupiedSlots() {
+        ArrayList<ArrayList<Integer>> expected=display.displayOccupied(parkingLot,3,parkingLot.getSlotsInEachFloor());
+        ArrayList<Integer> actual=new ArrayList<Integer>(Arrays.asList());
+        int floorNumber=0;
+        assertEquals(expected.get(floorNumber),actual);
+    }
+
 }
