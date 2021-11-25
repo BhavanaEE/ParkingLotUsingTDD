2665abe68826642e0af6012e432d719a69aa9ecb
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-11-25 11:17:08 +0530

    Checking number of floors for parking lot test

--- src/test/java/com/everest/parkinglot/ParkingLotTest.java
+++ src/test/java/com/everest/parkinglot/ParkingLotTest.java
@@ -1,6 +1,12 @@
 package com.everest.parkinglot;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +15,12 @@ class ParkingLotTest {
     private int noOfFloors=2;
     private int slotsInEachFloor=6;
     ParkingLot parkingLot;
+    @BeforeEach
+    public void setUp()
+    {
+        parkingLot = new ParkingLot("PR1234",2,6);
+    }
+
     @Test
     void checkNoOfFloorsTest(){
         int expected=parkingLot.getNoOfFloors();
